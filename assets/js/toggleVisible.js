1c161978cfa62542d53bd585cd5813bbd56ac537
Magomed <me@sekkeio.me>
2024-12-07 00:14:58 +0300

    add toggleVisible

---
+++ assets/js/toggleVisible.js
@@ -0,0 +1,4 @@
+document.getElementById('clickButton').addEventListener('click', () => {
+    document.getElementById('clickButton').classList.add('hide');
+    document.getElementById('result').classList.remove('hide');
+});
